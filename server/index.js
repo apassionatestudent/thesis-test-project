c779d3396cc688c64f91c6c8c80afc74fcc34727
Ricson Escalicas <ricson.escalicas@gmail.com>
2025-08-11 11:49:22 +0800

    updates

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 // server/index.js
 
-require("dotenv").config({ path: __dirname + "/.env" });
+// require("dotenv").config({ path: __dirname + "/.env" });
 const express = require('express');
 
 const app = express();
@@ -11,6 +11,14 @@ const PORT = process.env.PORT || 9000;
 app.get("/", (req, res) => {
     res.send("Hello World!");
   });
+
+app.get("/aboutus", (req, res) => {
+    res.send("here is about us");
+});
+
+app.get("/services", (req, res) => {
+    res.send("here is services");
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on the port  ${PORT}`);
